0cc1063dfcf838ecc915b227f4fd5136fbc146fc
santooooooo <santo.shunsuke@gmail.com>
2023-04-11 19:11:00 +0900

    #4 add copy clipboard func

--- src/URL/url.tsx
+++ src/URL/url.tsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   useCurrentLocation,
   useLatitude,
   useLongitude,
-  //useUrl,
 } from "./../DataProvider";
+import { TextField, Button, Box, Typography } from "@mui/material";
 
 export default function Url() {
   const { checked } = useCurrentLocation();
   const { latitude, setLatitude } = useLatitude();
   const { longitude, setLongitude } = useLongitude();
-  //const { url, setUrl, resetUrl } = useUrl();
+  const baseUrl: string = window.location.host + "/ar/";
+  //クリップボードにコピーした時の結果を表示
+  let [copied, setCopied] = useState(false);
+  const color = copied ? "success" : "primary";
 
+  //latitude & longitudeの値をセット
   if (checked) {
     const setCurrentPosition = (pos: any) => {
       const crd = pos.coords;
       setLatitude(crd.latitude);
-      setLongitude(crd.longitude);
+      setLongitude("/" + crd.longitude);
     };
 
     navigator.geolocation.getCurrentPosition(
@@ -28,10 +32,42 @@ export default function Url() {
 
   return (
     <div className="url">
-      <h1>This is Url Page</h1>
-      <h1>CurrentLocation: {checked.toString()}</h1>
-      <h1>Latitude: {latitude}</h1>
-      <h1>Longitude: {longitude}</h1>
+      <Typography variant="h5">Share URL with friends</Typography>
+      <Box
+        sx={{
+          maxWidth: 300,
+          display: "inline-block",
+          border: 2,
+          borderColor: "primary.main",
+          borderRadius: 2,
+          mt: 2,
+        }}
+      >
+        <TextField
+          value={baseUrl + latitude + longitude}
+          type="readOnly"
+          variant="filled"
+          size="small"
+        />
+        <Button
+          sx={{
+            mt: 1,
+          }}
+          color={color}
+          onClick={() => {
+            copyTextToClipboard(
+              baseUrl + latitude + longitude,
+              copied,
+              setCopied
+            );
+          }}
+        >
+          {copied ? "copied" : "copy"}
+        </Button>
+      </Box>
+      <Button color="primary" variant="contained" sx={{ mt: 2 }}>
+        Go to AR Page
+      </Button>
     </div>
   );
 }
@@ -45,3 +81,19 @@ const options = {
   timeout: 5000,
   maximumAge: 0,
 };
+
+//URLをクリップボードにコピー
+function copyTextToClipboard(
+  url: string,
+  copied: boolean,
+  setCopied: Function
+) {
+  navigator.clipboard
+    .writeText(url)
+    .then(() => {
+      setCopied(!copied);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
