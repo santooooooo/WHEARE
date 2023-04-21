b72b8ad4a2d1a40b49e041266b42668232d56764
santooooooo <santo.shunsuke@gmail.com>
2023-04-21 12:15:31 +0900

    #1 add button to go to form in top page

--- src/Top/top.tsx
+++ src/Top/top.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
 
 export default function Top() {
+  const navigate = useNavigate();
+
   return (
     <div className="top">
       <h1>This is Top Page</h1>
+      <Button onClick={() => navigate(`/form`)} sx={{ width: 1 / 1 }}>
+        Go to Form
+      </Button>
     </div>
   );
 }
